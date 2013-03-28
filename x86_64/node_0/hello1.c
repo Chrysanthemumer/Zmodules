1ae71613faeba64f538955b53b63337e1dad28d7
swang <will.sizhou.wang@gmail.com>
2013-03-28 15:32:51 -0500

    last version of hello1

--- x86_64/node_0/hello1.c
+++ x86_64/node_0/hello1.c
@@ -3,11 +3,20 @@
  */
  
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/init.h>
+
+#include <linux/sched.h>
+//MODULE_AUTHOR("Sizhou.Wang");
 
 static int hello_init(void)
 {
+  static char *whom = "world";
+  //module_param(whom, charp, S_IRUGO);  
   printk(KERN_ALERT "Hello kernel 1.\n");
+  //Print the current running process pid
+  printk(KERN_INFO "The process is \"%s\" (pid %i)\n", current->comm, current->pid);
+  printk(KERN_INFO "Whom is \"%s\" .", whom);
   return 0;
 }
 
@@ -18,3 +27,4 @@ static void hello_exit(void)
 
 module_init(hello_init);
 module_exit(hello_exit);
+
