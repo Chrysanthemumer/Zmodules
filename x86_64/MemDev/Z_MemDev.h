d6a1dcff23bd081ebf1bfb6df109a77aefa25506
swang <will.sizhou.wang@gmail.com>
2013-04-04 13:34:19 -0500

    MemDev started

---
+++ x86_64/MemDev/Z_MemDev.h
@@ -0,0 +1,45 @@
+/*
+ * Z_MemDev.h -- header for Memory Devices development modules
+ *
+ * version 0.1
+ * 
+ * Copyright (C) 2013 Sizhou Wang
+ * Copyright (C) 2010 IIT
+ * 
+ * The source code in this file can be freely used, copied, adapted and 
+ * redistributed in source or binary form, so long as an acknowledgement
+ * appears in derived source files. The citation should list that the code 
+ * comes from 'Sizhou Wang, Ilinois Institute of Technology'.
+ * No warrenty is attached.
+ */
+ 
+ #ifndef _Z_MEMDEV_H_
+ #define _Z_MEMDEV_H_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Version History:
+ *
+ * 0.1: initialization version.
+ */
+ 
+ #endif
